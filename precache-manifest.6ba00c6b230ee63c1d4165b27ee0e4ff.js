7edbadd5f8e978d9af6bd76f3b14899f25d0a5c2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-02 01:31:44 +0000

    Deploy river-wolf/trading-pattern to github.com/river-wolf/trading-pattern.git:gh-pages

---
+++ precache-manifest.6ba00c6b230ee63c1d4165b27ee0e4ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65df4531d6023e76656ea3843ab66fc7",
+    "url": "/trading-pattern/index.html"
+  },
+  {
+    "revision": "6864d8687ea7a01aedf0",
+    "url": "/trading-pattern/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "66febd8fac16a0ae2b71",
+    "url": "/trading-pattern/static/css/main.d9520b51.chunk.css"
+  },
+  {
+    "revision": "6864d8687ea7a01aedf0",
+    "url": "/trading-pattern/static/js/2.8c5dbdbb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/trading-pattern/static/js/2.8c5dbdbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66febd8fac16a0ae2b71",
+    "url": "/trading-pattern/static/js/main.e8afd030.chunk.js"
+  },
+  {
+    "revision": "6bf05182a38cf1dc51cd",
+    "url": "/trading-pattern/static/js/runtime-main.d6660036.js"
+  },
+  {
+    "revision": "93d6259b06b7b9a8db8f2ec8dfe71054",
+    "url": "/trading-pattern/static/media/menu.93d6259b.svg"
+  },
+  {
+    "revision": "ccf157afd05fe486dde2e9ceb9141f5c",
+    "url": "/trading-pattern/static/media/trend-down.ccf157af.svg"
+  },
+  {
+    "revision": "42dc61ccdfe3790f06cd6f29e275158a",
+    "url": "/trading-pattern/static/media/trend-up.42dc61cc.svg"
+  }
+]);
